edfc61ad6828b8128ae6277954888348e068ec30
nehru <nehrukadavath9676@gmail.com>
2023-03-08 15:43:12 +0530

    Admin Entity Class

---
+++ Hospital_Appointment_System/src/main/java/com/silchar/medicare/entity/Admin.java
@@ -0,0 +1,50 @@
+package com.silchar.medicare.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="admin")
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)	
+    int id;
+	private String username;
+    private String email;
+    private String mobile;
+    private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
+}
